4a6ef13a3a1affc6220f072ea3d59b1af8cd59f3
Luiz Luna <luizluna.dev@gmail.com>
2024-10-03 21:59:22 -0300

    Adiciona exercício de dados e variáveis

---
+++ src/Dados/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Luiz"
+	var idade uint8 = 20
+	var altura float32 = 1.75
+
+	fmt.Println("nome: ", name)
+	fmt.Println("idade: ", idade)
+	fmt.Println("altura: ", altura)
+}
